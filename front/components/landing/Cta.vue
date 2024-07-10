0fa74285afa60a7d90321d8c4c35ed3cc98659ce
Olivier PERDRIX <olivierperdrix@live.fr>
2024-07-10 23:51:05 +0200

    fix(home page): :bug: fix text mistakes

--- front/components/landing/Cta.vue
+++ front/components/landing/Cta.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center">
-    <h2 class="text-white text-3xl md:text-6xl">Réservez une Session One-to-One</h2>
+    <h2 class="text-white text-3xl md:text-6xl">Réservez une Session <br />One-to-One</h2>
     <p class="text-slate-500 mt-4 text-lg md:text-xl">
-      esoin d'une aide personnalisée ? Réservez une session one-to-one avec un expert via notre fonctionnalité de partage d'écran. Obtenez des conseils spécifiques et résolvez vos problèmes en temps réel.
+      Besoin d'une aide personnalisée ? Réservez une session one-to-one avec un expert via notre fonctionnalité de partage d'écran. Obtenez des conseils spécifiques et résolvez vos problèmes en temps réel.
     </p>
     <div class="flex mt-5">
       <LandingLink href="/experts" styleName="inverted">Commencez maintenant</LandingLink>
