591cc06f2ea1e9cbd708c5e8cc5d579c6b685fbc
Yvan <yvan.schmitt01@gmail.com>
2024-07-10 09:29:12 +0200

    profil

---
+++ front/pages/profil.vue
@@ -0,0 +1,24 @@
+<script setup>
+definePageMeta({
+  layout: "landing",
+});
+
+const user = await $fetch('/api/users/' + localStorage.getItem('userId'), {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json'
+    }
+});
+
+</script>
+
+<template>
+  <LandingContainer>
+    <LandingSectionhead>
+      <template v-slot:title>Profil</template>
+      <template v-slot:desc>&nbsp;</template>
+    </LandingSectionhead>
+    
+  </LandingContainer>
+</template>
