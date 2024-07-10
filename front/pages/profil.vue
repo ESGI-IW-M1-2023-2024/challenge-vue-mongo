5619c7506ee2616a68ba13ca80e51f43f5f536da
Yvan <yvan.schmitt01@gmail.com>
2024-07-10 11:13:50 +0200

    get user

--- front/pages/profil.vue
+++ front/pages/profil.vue
@@ -5,18 +5,36 @@ definePageMeta({
   layout: "landing",
 });
 
-const user = ref();
+const user = ref({});
 
-onMounted(() => {
-  console.log(localStorage.getItem('userId'));
-  console.log(localStorage.getItem('token'));
-  user = $fetch('/api/users/' + localStorage.getItem('userId'), {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${localStorage.getItem('token')}`,
-      'Content-Type': 'application/json'
+onMounted(async () => {
+  // const user = $fetch('http://localhost:3000/api/users/' + localStorage.getItem('userId'), {
+  //   method: 'GET',
+  //   headers: {
+  //     'Authorization': `Bearer ${localStorage.getItem('token')}`,
+  //     'Content-Type': 'application/json'
+  //   }
+  // });
+  try {
+    const response = await fetch('http://localhost:3000/api/users/' + localStorage.getItem('userId'), {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      user.value = data;
+    } else {
+      const data = await response.json();
+      errorMessage.value = data.message;
     }
-  });
+  } catch (error) {
+    console.error(error);
+  }
 })
 </script>
 
@@ -26,6 +44,6 @@ onMounted(() => {
       <template v-slot:title>Profil</template>
       <template v-slot:desc>&nbsp;</template>
     </LandingSectionhead>
-    
+    {{ user.firstName }}
   </LandingContainer>
 </template>
