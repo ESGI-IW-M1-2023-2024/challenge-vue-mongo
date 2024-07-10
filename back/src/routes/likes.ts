4f395f7368de15b89f4047671ff882b930048c63
Olivier PERDRIX <olivierperdrix@live.fr>
2024-07-10 11:24:10 +0200

    feat(CRUD): :recycle: refactor likes CRUD

--- back/src/routes/likes.ts
+++ back/src/routes/likes.ts
@@ -1,55 +1,42 @@
 import { Hono } from 'hono';
-import { Like } from '../models/like';
+import { User } from '../models/user';
+import { isValidObjectId } from 'mongoose';
 
-const api = new Hono().basePath('/likes');
+const api = new Hono().basePath('/users/:idUser/likes');
+
+const getUser = async (c: any) => {
+  const idUser = c.req.param('idUser');
+  if (!isValidObjectId(idUser)) {
+    throw new Error("Invalid user id");
+  }
+  const user = await User.findById(idUser);
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user;
+}
 
 api.get('/', async (c) => {
-  const users = await Like.find();
-  return c.json(users, 200);
+  const user = await getUser(c);
+  if (!user) {
+    return c.json({ message: 'User not found' }, 404);
+  }
+  const likes = user.likes;
+  return c.json(likes, 200);
 });
 
 api.get('/:id', async (c) => {
+  const user = await getUser(c);
+  if (!user) {
+    return c.json({ message: 'User not found' }, 404);
+  }
+
   const { id } = c.req.param();
-  const like = await Like.findById(id);
+  const like = user.likes.find((like: any) => like._id == id);
   if (!like) {
     return c.json({ message: 'Like not found' }, 404);
   }
   return c.json(like, 200);
-});
-
-api.post('/', async (c) => {
-  const { idUser, score } = await c.req.json();
-  try {
-    const like = new Like({ idUser, score });
-    await like.save();
-    return c.json(like, 201);
-  } catch (err: any) {
-    return c.json({ message: 'Error creating like', error: err.message }, 400);
-  }
-});
-
-api.patch('/:id', async (c) => {
-  const { id } = c.req.param();
-  const { score } = await c.req.json();
-  try {
-    const like = await Like.findByIdAndUpdate(id, {
-      score
-    }, { new: true });
-    return c.json(like, 200);
-  }
-  catch (err: any) {
-    return c.json({ message: 'Error updating like', error: err.message }, 400);
-  }
-});
-
-api.delete('/:id', async (c) => {
-  const { id } = c.req.param();
-  try {
-    await Like.findByIdAndDelete(id);
-    return c.json({ message: 'Like deleted successfully' }, 200);
-  } catch (err: any) {
-    return c.json({ message: 'Error deleting like', error: err.message }, 400);
-  }
 });
 
 export default api;
