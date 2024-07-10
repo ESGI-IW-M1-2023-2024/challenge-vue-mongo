329968bcac97b522fce460834d07fd2aebf1951d
Olivier PERDRIX <olivierperdrix@live.fr>
2024-07-10 23:44:09 +0200

    feat(home page): :sparkles: add homepage texts

--- front/components/landing/Cta.vue
+++ front/components/landing/Cta.vue
@@ -1,13 +1,11 @@
 <template>
-  <div
-    class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
-  >
-    <h2 class="text-white text-3xl md:text-6xl">Build faster websites.</h2>
+  <div class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center">
+    <h2 class="text-white text-3xl md:text-6xl">Réservez une Session One-to-One</h2>
     <p class="text-slate-500 mt-4 text-lg md:text-xl">
-      Static or Dynamic, the choice is yours with Nuxt's hybrid rendering modes.
+      esoin d'une aide personnalisée ? Réservez une session one-to-one avec un expert via notre fonctionnalité de partage d'écran. Obtenez des conseils spécifiques et résolvez vos problèmes en temps réel.
     </p>
     <div class="flex mt-5">
-      <LandingLink href="#" styleName="inverted">Get Started</LandingLink>
+      <LandingLink href="/experts" styleName="inverted">Commencez maintenant</LandingLink>
     </div>
   </div>
 </template>
