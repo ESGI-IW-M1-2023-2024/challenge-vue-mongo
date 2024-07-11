1b0b456ee2f4b95a4a753add52d10118da6a3cd8
loan <loan.courchinoux-billonnet@koesio.com>
2024-07-12 01:07:12 +0200

    feat: :sparkles: websocket back

--- back/src/index.ts
+++ back/src/index.ts
@@ -11,6 +11,7 @@ import like from './routes/likes';
 import message from './routes/messages';
 import comments from './routes/comments';
 import issues from './routes/issues';
+import { WebSocketServer } from 'ws';
 
 const app = new Hono();
 const port = myEnv.port;
@@ -18,6 +19,10 @@ await DbConnect();
 console.log(`🚀 Server is running on http://localhost:${port}`);
 
 app.use('/api/*', cors());
+app.use('*', async (c, next) => {
+  await next();
+  console.info(`[${c.req.method}] ${c.req.path} - ${c.res.status}`);
+});
 
 const routes = [users, technologies, auth, comments, like, message, issues];
 
@@ -29,7 +34,20 @@ app.use('*', async (c) => {
   c.json({ msg: '404 not found' });
 });
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port,
+});
+
+// @ts-ignore
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+  ws.on('message', async function message(data) {
+    console.log(data);
+    if (ws.readyState === ws.OPEN) {
+      ws.send(data);
+    }
+  });
 });
