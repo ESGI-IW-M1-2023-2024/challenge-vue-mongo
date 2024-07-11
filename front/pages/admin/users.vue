3a020578b1b2c49c16d2e3cbb147dc451962b047
Yvan <yvan.schmitt01@gmail.com>
2024-07-11 14:27:21 +0200

    add new user

--- front/pages/admin/users.vue
+++ front/pages/admin/users.vue
@@ -6,28 +6,30 @@ definePageMeta({
 });
 
 const users = ref([]);
-const selectedUser = reactive({ _id: null, firstName: '', lastName: '', email: '', role: '' });
-const newUser = reactive({ firstName: '', lastName: '', email: '', role: '', password: '' });
+const selectedUser = reactive({ _id: null, firstName: '', lastName: '', email: '', password: '', gender: '', role: '' });
+const newUser = reactive({ firstName: '', lastName: '', email: '', role: '', password: '', gender: '' });
 const userDialog = ref(null);
+const newUserModal = ref(null);
 const errorMessage = ref('');
 
 
-const openModal = (user) => {
+const openUserModal = (user) => {
   Object.assign(selectedUser, user);
+  selectedUser.password = '';
   userDialog.value.showModal();
 };
 
-const closeModal = () => {
-    userDialog.value.close();
+const closeUserModal = () => {
+  userDialog.value.close();
 };
 
-const newUserModal = (user) => {
-  Object.assign(selectedUser, user);
-  userDialog.value.showModal();
+const openNewUserModal = () => {
+  newUserModal.value.showModal();
+  Object.assign(newUser, { firstName: '', lastName: '', email: '', role: '', password: '', gender: '' });
 };
 
-const newUserModal = () => {
-    userDialog.value.close();
+const closeNewUserModal = () => {
+  newUserModal.value.close();
 };
 
 const loadUserList = async () => {
@@ -54,13 +56,14 @@ const loadUserList = async () => {
 
 const deleteUser = async (user) => {
   try {
-    const response = await fetch('http://localhost:3000/api/users/${user.id}', {
+    const response = await fetch('http://localhost:3000/api/users/' + user._id, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`,
         'Content-Type': 'application/json',
       },
     });
+    loadUserList();
   } catch (error) {
     console.error('Error deleting user:', error);
   }
@@ -75,12 +78,22 @@ const saveUser = async () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(
-        {
-          firstName: selectedUser.firstName,
-          lastName: selectedUser.lastName,
-          email: selectedUser.email,
-          role: selectedUser.role,
-        }
+        selectedUser.password.length > 0
+          ? {
+              firstName: selectedUser.firstName,
+              lastName: selectedUser.lastName,
+              gender: selectedUser.gender,
+              email: selectedUser.email,
+              role: selectedUser.role,
+              password: selectedUser.password,
+            }
+          : {
+            firstName: selectedUser.firstName,
+            lastName: selectedUser.lastName,
+            gender: selectedUser.gender,
+            email: selectedUser.email,
+            role: selectedUser.role,
+          }
       ),
     });
 
@@ -99,23 +112,25 @@ const saveUser = async () => {
 
 const addUser = async () => {
   try {
-    const response = await fetch('http://localhost:3000/api/register', {
+    const response = await fetch('http://localhost:3000/api/users', {
       method: 'POST',
       headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        firstName: selectedUser.firstName,
-        lastName: selectedUser.lastName,
-        email: selectedUser.email,
-        role: selectedUser.role,
-        password: password.value,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        gender: newUser.gender,
+        email: newUser.email,
+        password: newUser.password,
+        role: newUser.role,
       }),
     });
 
     if (response.ok) {
       loadUserList();
-      userDialog.value.close();
+      newUserModal.value.close();
     } else {
       const data = await response.json();
       errorMessage.value = data.message;
@@ -141,7 +156,7 @@ onMounted(() => {
   <div class="p-4">
     <div class="flex justify-between items-center p-2">
       <h1 class="text-2xl font-bold mb-4">Liste des Utilisateurs</h1>
-      <LandingButton @click="openModal()" size="md">Nouvel Utilisateur</LandingButton>
+      <LandingButton @click="openNewUserModal()" size="md">Nouvel Utilisateur</LandingButton>
     </div>
     <ul class="space-y-2">
       <li v-for="user in users" :key="user.id" class="flex justify-between items-center p-2 border rounded">
@@ -151,7 +166,7 @@ onMounted(() => {
         </div>
         <div>
           <button @click="deleteUser(user)" class="ml-4 bg-red-500 text-white px-2 py-1 rounded">Supprimer</button>
-          <button @click="openModal(user)" class="ml-4 bg-blue-500 text-white px-2 py-1 rounded">Modifier</button>
+          <button @click="openUserModal(user)" class="ml-4 bg-blue-500 text-white px-2 py-1 rounded">Modifier</button>
         </div>
       </li>
     </ul>
@@ -168,50 +183,31 @@ onMounted(() => {
           <input v-model="selectedUser.lastName" class="w-full p-2 border rounded" required>
         </div>
         <div>
-          <label class="block">Email:</label>
-          <input v-model="selectedUser.email" class="w-full p-2 border rounded" required type="email">
-        </div>
-        <div>
-          <label class="block">Role:</label>
-          <select class="mt-3 block w-full py-1 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" v-model="selectedUser.role">
-            <option value="admin">Admin</option>
-            <option value="user">User</option>
-            <option value="mentor">Mentor</option>
+          <label class="block">Genre:</label>
+          <select class="mt-3 block w-full py-1 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" v-model="selectedUser.gender">
+            <option value="male">Homme</option>
+            <option value="female">Femme</option>
+            <option value="other">Autre</option>
           </select>
-        </div>
-        <div class="flex justify-end space-x-2">
-          <button @click="closeModal" class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
-          <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Sauvegarder</button>
-        </div>
-        <div id="result" class="mt-3 text-center">{{ errorMessage }}</div>
-      </form>
-    </dialog>
-
-    <dialog ref="addUserDialog" class="p-6 border rounded-md bg-white">
-      <form method="dialog" @submit.prevent="saveUser" class="space-y-4">
-        <h2 class="text-xl font-bold">Nouvel Utilisateur</h2>
-        <div>
-          <label class="block">Prénom:</label>
-          <input v-model="selectedUser.firstName" class="w-full p-2 border rounded" required>
-        </div>
-        <div>
-          <label class="block">Nom:</label>
-          <input v-model="selectedUser.lastName" class="w-full p-2 border rounded" required>
         </div>
         <div>
           <label class="block">Email:</label>
           <input v-model="selectedUser.email" class="w-full p-2 border rounded" required type="email">
         </div>
         <div>
+          <label class="block">Mot de passe:</label>
+          <input v-model="selectedUser.password" class="w-full p-2 border rounded" type="password">
+        </div>
+        <div>
           <label class="block">Role:</label>
           <select class="mt-3 block w-full py-1 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" v-model="selectedUser.role">
-            <option value="admin">Admin</option>
             <option value="user">User</option>
             <option value="mentor">Mentor</option>
+            <option value="admin">Admin</option>
           </select>
         </div>
         <div class="flex justify-end space-x-2">
-          <button @click="closeModal" class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
+          <button type="button" @click="closeUserModal" class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
           <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Sauvegarder</button>
         </div>
         <div id="result" class="mt-3 text-center">{{ errorMessage }}</div>
@@ -219,15 +215,23 @@ onMounted(() => {
     </dialog>
 
     <dialog ref="newUserModal" class="p-6 border rounded-md bg-white">
-      <form method="dialog" @submit.prevent="saveUser" class="space-y-4">
-        <h2 class="text-xl font-bold">Modifier Utilisateur</h2>
+      <form method="dialog" @submit.prevent="addUser" class="space-y-4">
+        <h2 class="text-xl font-bold">Nouvel Utilisateur</h2>
         <div>
           <label class="block">Prénom:</label>
-          <input v-model="selectedUser.firstName" class="w-full p-2 border rounded" required>
+          <input v-model="newUser.firstName" class="w-full p-2 border rounded" required>
         </div>
         <div>
           <label class="block">Nom:</label>
           <input v-model="newUser.lastName" class="w-full p-2 border rounded" required>
+        </div>
+        <div>
+          <label class="block">Genre:</label>
+          <select class="mt-3 block w-full py-1 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" v-model="newUser.gender">
+            <option value="male" selected>Homme</option>
+            <option value="female">Femme</option>
+            <option value="other">Autre</option>
+          </select>
         </div>
         <div>
           <label class="block">Email:</label>
@@ -235,18 +239,18 @@ onMounted(() => {
         </div>
         <div>
           <label class="block">Mot de passe:</label>
-          <input v-model="newUser.password" class="w-full p-2 border rounded" required type="email">
+          <input v-model="newUser.password" class="w-full p-2 border rounded" required type="password">
         </div>
         <div>
           <label class="block">Role:</label>
           <select class="mt-3 block w-full py-1 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" v-model="newUser.role">
-            <option value="admin">Admin</option>
-            <option value="user">User</option>
+            <option value="user" selected>User</option>
             <option value="mentor">Mentor</option>
+            <option value="admin">Admin</option>
           </select>
         </div>
         <div class="flex justify-end space-x-2">
-          <button @click="closeModal" class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
+          <button type="button" @click="closeNewUserModal" class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
           <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Sauvegarder</button>
         </div>
         <div id="result" class="mt-3 text-center">{{ errorMessage }}</div>
